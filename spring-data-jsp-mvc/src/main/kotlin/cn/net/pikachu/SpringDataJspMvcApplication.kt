3a2b195634f9ce7e579da3fa91b639b0ca74c001
PikachuHy <2931408816@qq.com>
2017-04-22 21:51:26 +0800

    终极版宠物商店

---
+++ spring-data-jsp-mvc/src/main/kotlin/cn/net/pikachu/SpringDataJspMvcApplication.kt
@@ -0,0 +1,11 @@
+package cn.net.pikachu
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class SpringDataJspMvcApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(SpringDataJspMvcApplication::class.java, *args)
+}
