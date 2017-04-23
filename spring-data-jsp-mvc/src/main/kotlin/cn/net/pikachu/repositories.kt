de79be4a69d607e437285146500e297dc64ee23d
PikachuHy <2931408816@qq.com>
2017-04-23 17:47:28 +0800

    添加dao

--- spring-data-jsp-mvc/src/main/kotlin/cn/net/pikachu/repositories.kt
+++ spring-data-jsp-mvc/src/main/kotlin/cn/net/pikachu/repositories.kt
@@ -1,5 +1,26 @@
 package cn.net.pikachu
 
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
 /**
  * Created by pikachu on 2017/4/22.
  */
+@Repository
+interface AccountRepository : JpaRepository<Account, Long>
+@Repository
+interface CategoryRepository : JpaRepository<Category, Long>
+@Repository
+interface ItemRepository : JpaRepository<Item, Long>
+@Repository
+interface OrderRepository : JpaRepository<Order,Long>
+@Repository
+interface CommentRepository : JpaRepository<Comment,Long>
+@Repository
+interface CartRepository : JpaRepository<Cart,Long>
+@Repository
+interface SupplierRepository : JpaRepository<Supplier,Long>
+@Repository
+interface PayInfoRepository : JpaRepository<PayInfo,Long>
+@Repository
+interface LogisticsInfoRepository : JpaRepository<LogisticsInfo,Long>
