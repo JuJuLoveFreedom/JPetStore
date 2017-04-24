a3ae205aeee6036522d46a13fb2fd3b07a630732
JuJuLoveFreedom <786549697@qq.com>
2017-04-24 19:17:39 +0800

    编写部分测试

--- spring-data-jsp-mvc/src/test/kotlin/cn/net/pikachu/ServicesTests.kt
+++ spring-data-jsp-mvc/src/test/kotlin/cn/net/pikachu/ServicesTests.kt
@@ -1,21 +1,40 @@
 package cn.net.pikachu
 
+import org.junit.Assert
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.junit4.SpringRunner
+/*
 
 @RunWith(SpringRunner::class)
 @SpringBootTest
 class AccountServiceTest {
+    @Autowired
     lateinit var accountService: AccountService
-
+    @Autowired
+    lateinit var userRepository: UserRepository
+    val address = Address("中国","浑南","长沙","天心区","415129")
+    val accountBaseInfo = AccountBaseInfo(0,"15084909943","2931408816","2931408816@qq.com","杨浩","大黄老鼠")
     // 登录
-    fun testSignIn(user: User){
-        
+    fun testSignIn_true(user: User){
+        val user = User(0,"pikachu","123",)
+        val flag =  accountService.signIn(user)
+        Assert.assertTrue(flag)
+    }
+    fun testSignIn_false(user:User){
+        val user = User(0,"pikachu","1234",null)
+        val flag =  accountService.signIn(user)
+        Assert.assertFalse(flag)
     }
     // 注册
-    fun testSignUp(user: User)
+    fun testSignUp(user: User){
+        val user = User(0,"test","test",null)
+        val u = accountService.signUp(user)
+        val uu = userRepository.findOne(u.id)
+        Assert.assertEquals(uu,u)
+    }
     // 找回密码
     fun testFindPassword(username:String,verificationCode:String)
     // 获取个人信息
@@ -37,3 +56,4 @@ class AccountServiceTest {
     // 获取喜欢的商品
     fun testGetFavItems(id: Long)
 }
+*/
